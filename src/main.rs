a8f29c50b9cf64355958a7e5274655f30c5e9742
AllLiver <97857498+AllLiver@users.noreply.github.com>
2024-04-09 10:10:37 -0600

    Fixed a small bug

--- src/main.rs
+++ src/main.rs
@@ -53,6 +53,8 @@ fn main() -> Result<()> {
         buf[0].push(' ');
     }
 
+    dbg!(&buf);
+
     // Switch terminal modes
     crossterm::terminal::enable_raw_mode().context("Could not enable raw mode")?;
     stdout()
@@ -122,7 +124,9 @@ fn main() -> Result<()> {
                     KeyCode::Up => {
                         if term.pos.y != 2 {  
                             term.move_relative(0, -1);
-                            if buf[(term.pos.y - 2) as usize].len() < buf_x_pos as usize {
+                            if buf[(term.pos.y - 2) as usize].is_empty() {
+                                term.move_cursor(0, term.pos.y);
+                            } else if buf[(term.pos.y - 2) as usize].len() < buf_x_pos as usize {
                                 term.move_relative((buf[(term.pos.y - 2) as usize].len() as i16) - (buf_x_pos as i16), 0)
                             } else {
                                 term.move_cursor(buf_x_pos, term.pos.y);
@@ -132,7 +136,9 @@ fn main() -> Result<()> {
                     KeyCode::Down => {
                         if term.pos.y != term.size.y - 3 && term.pos.y - 1 < buf.len() as u16 {
                             term.move_relative(0, 1);
-                            if buf[(term.pos.y - 2) as usize].len() < buf_x_pos as usize {
+                            if buf[(term.pos.y - 2) as usize].is_empty() {
+                                term.move_cursor(0, term.pos.y);
+                            } else if buf[(term.pos.y - 2) as usize].len() < buf_x_pos as usize {
                                 term.move_relative((buf[(term.pos.y - 2) as usize].len() as i16) - (buf_x_pos as i16), 0)
                             } else {
                                 term.move_cursor(buf_x_pos, term.pos.y);
